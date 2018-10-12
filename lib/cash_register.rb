b979b73537f75e018802a1cbd62837eee2464d69
Edgar Irizarry <eirizarry15@yahoo.com>
2018-10-12 21:52:00 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :price, :items 
+  
+def initalize(discount = 0)
+  @total = 0 
+  @discount = discount 
+  @items = []
+end 
+
 
